c455580491cd95936061a027d437369a1778fb9a
Jay Feldis <jfeldis@bitseed.org>
2017-11-27 21:47:20 -0800

    Create install.sh

---
+++ setup/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#install Bitseed V3 GUI for Bitcoin Core bitcoind
+#Ubuntu 16 supported
+
+#dependencies
+sudo apt-get update
+sudo apt-get install apache2 libapache2-mod-php php_mcrypt php-mysql php-curl
