5c8597480a48fe0892c8e573e30313904c175788
Jay Feldis <jfeldis@bitseed.org>
2017-11-27 22:13:06 -0800

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -4,4 +4,14 @@
 
 #dependencies
 sudo apt-get update
-sudo apt-get install apache2 libapache2-mod-php php_mcrypt php-mysql php-curl
+sudo apt-get install apache2 libapache2-mod-php php-mysql php-curl php-mcrypt
+
+#Apache config
+sudo cp $HOME/bitseed-web-ui-2/000-default.conf  /etc/apache2/sites-enabled/
+sudo cp  $HOME/bitseed-web-ui-2/ports.conf  /etc/apache2/
+sudo chmod 666 /var/www/html/*.txt
+
+
+
+sudo chmod 666 $HOME/.bitcoin/bitcoin.conf
+
