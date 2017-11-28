428970ed4fafb651907e0cbb633255583cbf238e
Jay Feldis <jfeldis@bitseed.org>
2017-11-27 22:46:17 -0800

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -4,12 +4,18 @@
 
 #dependencies
 sudo apt-get update
-sudo apt-get -y install apache2 libapache2-mod-php php-mysql php-curl php-mcrypt dnsutils
+sudo apt-get -y install apache2 libapache2-mod-php php-mysql php-curl php-mcrypt dnsutils ntp
+
+#set serial number
+echo "Enter device serial number:"
+read serial
+echo $serial > $HOME/"deviceid-$serial"
 
 #Apache config
 sudo cp $HOME/bitseed-web-ui-2/000-default.conf  /etc/apache2/sites-enabled/
 sudo cp  $HOME/bitseed-web-ui-2/ports.conf  /etc/apache2/
 sudo cp -r $HOME/bitseed-web-ui-2/html/* /var/www/html
+sudo echo $serial > /var/www/html/serial
 sudo chmod 666 /var/www/html/*.txt
 sudo chown -R www-data:www-data /var/www
 
@@ -37,3 +43,15 @@ sudo cp $HOME/bitcoin-box/.hdd/bitseed.conf $HOME/.bitseed
 sudo echo "211" > $HOME/version
 sudo /etc/init.d/apache2 restart
 echo "web admin install done" > $HOME/bitcoin-box/setup/setup.log
+
+#Tor setup
+sudo apt-get install -y tor
+sudo echo "HiddenServiceDir /var/lib/tor/bitseed-service/" >> /etc/tor/torrc
+sudo echo "HiddenServicePort 80 127.0.0.1:82" >> /etc/tor/torrc
+sudo echo "ControlPort 9051" >> /etc/tor/torrc
+sudo echo "CookieAuthentication 1" >> /etc/tor/torrc
+sudo usermod -a -G debian-tor $USER 
+sudo service tor restart
+sleep 5
+sudo cat /var/lib/tor/bitseed-service/hostname
+echo "setup done.  reboot"
