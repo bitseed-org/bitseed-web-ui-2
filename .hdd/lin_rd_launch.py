f129555020863d8f258d00169f2d36826c74dff2
kdanley42 <kdanley42@gmail.com>
2017-12-04 07:34:01 -0800

    Update lin_rd_launch.py
    Cleaned up comments

--- .hdd/lin_rd_launch.py
+++ .hdd/lin_rd_launch.py
@@ -4,8 +4,7 @@
 #  Created:     Konn Danley
 #  File:        lin_rd_launch.py
 #  Date:        05/15/2016
-#  Purpose:     This script resides in /home/linaro.  
-#               It detects a '1' written to rd_bconf_flag.  
+#  Purpose:     This script detects a '1' written to rd_bconf_flag.  
 #               If a '1' is detected, then the lin_rd_bconf_mbox.py
 #               script will be run.
 #
