1dbaf78437baed81b7648de34a46b27d2254af38
Jay Feldis <jfeldis@bitseed.org>
2017-12-24 14:48:40 -0800

    Update safestop.sh

--- .hdd/safestop.sh
+++ .hdd/safestop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #stops bitcoind and shuts down device when /home/bitcoin/retartflag is set to 2
 #intended to be run by sudo crontab -e  (root crontab) every minute
-rsflag=$( < $HOME/restartflag)
+rsflag=$( < /home/bitcoin/restartflag)
 #echo "Flag= $rsflag"
 if (( rsflag == 2 )); then
  echo "stopiing bitcoind, please wait"
-echo 0 > $HOME/restartflag
+echo 0 > /home/bitcoin/restartflag
 /home/bitcoin/bitcoin-cli stop
 echo "Do not shut down the device until notified"
 $x=$(pgrep -f bitcoind)
@@ -16,7 +16,7 @@ do
   x=$(pgrep -f bitcoind)
 done
 echo "bitcoin has stopped. shutdown in 5 seconds"
-echo "(date) bitcoind stopped via safestop.sh" >> $HOME/.bitseed/bitseed.log
+echo "(date) bitcoind stopped via safestop.sh" >> /home/bitcoin/.bitseed/bitseed.log
 sleep 10s
 sudo shutdown -h now
 fi
