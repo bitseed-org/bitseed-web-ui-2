91ebc599c826241c3774d91e1da6d6b950536380
Jay Feldis <jfeldis@bitseed.org>
2017-12-25 00:55:54 -0800

    individualize cloned V3

---
+++ setup/clonefix.sh
@@ -0,0 +1,17 @@
+#run this as sudo on a new cloned V3
+#1) new onion address
+#2) mount new swap UUID
+#3) new serial number
+
+#new tor address
+sudo rm /var/lib/tor/bitseed-service/hostname
+sudo rm /var/lib/tor/bitseed-service/hostname
+/etc/init.d/tor restart
+
+#new serial number
+rm $HOME/deviceid*
+echo "Enter device serial number:"
+read serial
+echo $serial > $HOME/"deviceid-$serial"
+echo $serial > /var/www/html/serial
+echo $serial > /var/www/onion/serial
