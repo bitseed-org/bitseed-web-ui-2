a1866572e153503d999a334fec3004b5d64b9e48
Jay Feldis <jfeldis@bitseed.org>
2017-12-30 19:24:26 -0800

    Update deviceset.sh

--- setup/deviceset.sh
+++ setup/deviceset.sh
@@ -18,3 +18,9 @@ sudo mkdir /home/bitcoin/temp
 sudo mount /dev/sdc1 /home/bitcoin/temp
 sudo chown -R bitcoin:bitcoin /home/bitcoin/temp
 bash /home/bitcoin/temp/importchain.sh
+
+#change host name
+sudo hostname bitseed3
+sudo rm /etc/hostname
+sudo echo "bitseed3" >> /etc/hostname
+sudo sed -i "s/ubuntu/bitseed3/g" /etc/hosts
