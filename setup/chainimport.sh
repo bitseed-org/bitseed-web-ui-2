93f2361d584c198de1162193ebbd4d3e4ab55bce
Jay Feldis <jfeldis@bitseed.org>
2018-01-04 01:21:28 -0800

    Update chainimport.sh

--- setup/chainimport.sh
+++ setup/chainimport.sh
@@ -1,2 +1,7 @@
 #pull blockchain over local network
+#sudo mkdir /home/bitcoin/temp
+#sudo mount /dev/sdc1 /home/bitcoin/temp
+#sudo chown -R bitcoin:bitcoin /home/bitcoin/temp
+#bash /home/bitcoin/temp/importchain.sh
+
 rsync -r --info=progress2 storage@10.1.10.121:/home/storage/disk3/.bitcoin /home/bitcoin
