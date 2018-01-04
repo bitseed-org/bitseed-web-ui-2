901a5d90869f9e95cfdc13428b32ff542accff12
Jay Feldis <jfeldis@bitseed.org>
2018-01-04 01:11:02 -0800

    Update deviceset.sh

--- setup/deviceset.sh
+++ setup/deviceset.sh
@@ -5,6 +5,12 @@
 #ssh access
 sudo apt-get install openssh-server
 
+#change host name
+sudo hostname bitseed3
+sudo rm /etc/hostname
+sudo echo "bitseed3" >> /etc/hostname
+sudo sed -i "s/ubuntu/bitseed3/g" /etc/hosts
+
 #new user
 sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/bitcoin --gecos "User" bitcoin
 echo 'bitcoin:bitcoin' | sudo chpasswd
@@ -16,20 +22,14 @@ sudo userdel -r oem
 
 
 #copy blockchain
-sudo mkdir /home/bitcoin/temp
-sudo mount /dev/sdc1 /home/bitcoin/temp
-sudo chown -R bitcoin:bitcoin /home/bitcoin/temp
-bash /home/bitcoin/temp/importchain.sh
-
-#change host name
-sudo hostname bitseed3
-sudo rm /etc/hostname
-sudo echo "bitseed3" >> /etc/hostname
-sudo sed -i "s/ubuntu/bitseed3/g" /etc/hosts
+#sudo mkdir /home/bitcoin/temp
+#sudo mount /dev/sdc1 /home/bitcoin/temp
+#sudo chown -R bitcoin:bitcoin /home/bitcoin/temp
+#bash /home/bitcoin/temp/importchain.sh
 
 
 #Finish
-https://github.com/bitseed-org/bitseed-web-ui-2.git
-sudo umount /home/bitcoin/temp
+#https://github.com/bitseed-org/bitseed-web-ui-2.git
+#sudo umount /home/bitcoin/temp
 
 echo "device setup complete"
